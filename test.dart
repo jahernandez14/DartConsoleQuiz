2e36257c530e9b04db1152b61f537d27cffbed07
jahernandez14 <jahernandez14@miners.utep.edu>
2021-09-21 13:13:42 -0600

    server connection added

--- test.dart
+++ test.dart
@@ -1,3 +1,13 @@
-void main() {
-  print("Test".substring(0, 1));
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() async {
+  const _Query = 'quiz';
+  var url = Uri.http(
+      'www.cs.utep.edu', '/cheon/cs4381/homework/quiz', {_Query: "quiz01"});
+  var response = await http.get(url);
+  final query = response.body;
+  print(query);
+  var decoded = json.decode(query);
+  print(decoded);
 }
